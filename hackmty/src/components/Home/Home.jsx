24256149724229bb363cb1685c5bc91cda91c214
maikyh <miguelgrza.12@gmail.com>
2023-09-23 14:54:58 -0600

    Starting project

---
+++ hackmty/src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState, useEffect, Suspense } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default function Home () {
+    
+    return (
+        <div className="home">
+            hello
+        </div>
+    );
+}
